55d9a6bc06abd177c42417000e0a3d1164bdd39c
njs <njs@pobox.com>
2005-01-18 11:09:27 +0000

    2005-01-18  Nathaniel Smith  <njs@codesourcery.com>
    	* vocab.hh: Remove file<cert>.
    	* vocab.cc: Likewise.
    	* packet_types.hh: Remove file.
    	* Makefile.am (MOST_SOURCES): Remove packet_types.hh and mac.hh.

--- netsync.cc
+++ netsync.cc
@@ -443,8 +443,6 @@ session::session(protocol_role role,
   prng.reset(new CryptoPP::AutoSeededRandomPool(request_blocking_rng));
 
   done_refinements.insert(make_pair(rcert_item, done_marker()));
-  done_refinements.insert(make_pair(mcert_item, done_marker()));
-  done_refinements.insert(make_pair(fcert_item, done_marker()));
   done_refinements.insert(make_pair(key_item, done_marker()));
   
   requested_items.insert(make_pair(rcert_item, boost::shared_ptr< set<id> >(new set<id>())));
@@ -2283,6 +2281,8 @@ session::process_data_cmd(netcmd_item_type type,
 
   // it's ok if we received something we didn't ask for; it might
   // be a spontaneous transmission from refinement
+  // FIXME: what does the above comment mean?  note_item_arrived does require
+  // that the item passed to it have been requested...
   note_item_arrived(type, item);
                            
   switch (type)
@@ -2411,6 +2411,8 @@ session::process_delta_cmd(netcmd_item_type type,
 
   // it's ok if we received something we didn't ask for; it might
   // be a spontaneous transmission from refinement
+  // FIXME: what does the above comment mean?  note_item_arrived does require
+  // that the item passed to it have been requested...
   note_item_arrived(type, ident);
 
   switch (type)
