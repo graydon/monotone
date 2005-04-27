fcfb93372cd0305a1fc68393a629da1d9555a71d
njs <njs@pobox.com>
2005-04-27 04:30:52 +0000

    2005-04-26  Nathaniel Smith  <njs@codesourcery.com>
    	* manifest.cc, inodeprint.cc: Don't include boost/regex.hpp.

--- manifest.cc
+++ manifest.cc
@@ -10,8 +10,6 @@
 #include <algorithm>
 #include <iterator>
 
-#include <boost/regex.hpp>
-
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/convenience.hpp>
