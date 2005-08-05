f178d171ec92476ac167af90d811f7e82d8123d8
njs <njs@pobox.com>
2005-08-05 00:25:20 +0000

    2005-08-04  Nathaniel Smith  <njs@pobox.com>
    	* netsync.cc (rebuild_merkle_trees): Tweak message ("rebuilding
    	merkle trees" does not mean anything to J. Random User...)

--- netsync.cc
+++ netsync.cc
@@ -3623,7 +3623,7 @@ void
 session::rebuild_merkle_trees(app_state & app,
                               set<utf8> const & branchnames)
 {
-  P(F("rebuilding merkle trees ...\n"));
+  P(F("calculating synchronization set ...\n"));
   for (set<utf8>::const_iterator i = branchnames.begin();
       i != branchnames.end(); ++i)
     L(F("including branch %s") % *i);
