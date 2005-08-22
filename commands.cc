c2c0305e245cb843f25f052c17d365535d049084
njs <njs@pobox.com>
2005-08-22 10:49:19 +0000

    2005-08-22  Nathaniel Smith  <njs@pobox.com>
    	* monotone.cc (coptions, options): Use gettext_noop to mark usage
    	strings for i18n.
    	* commands.cc (commands): gettextify command descriptions
    	* po/Makevars (XGETTEXT_OPTIONS): Include the 2nd, 3rd, and 4th
    	arguments to CMD macro as translatedable strings.

--- commands.cc
+++ commands.cc
@@ -21,6 +21,8 @@
 #include <boost/filesystem/exception.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+#include "gettext.h"
+
 #include "commands.hh"
 #include "constants.hh"
 
@@ -113,7 +115,8 @@ namespace commands
             string const & p,
             string const & d,
             command_opts const & o)
-      : name(n), cmdgroup(g), params(p), desc(d), options(o)
+      : name(n), cmdgroup(gettext(g)), params(gettext(p)), desc(gettext(d)),
+        options(o)
     { cmds[n] = this; }
     virtual ~command() {}
     virtual void exec(app_state & app, vector<utf8> const & args) = 0;
