de872c9fba250d2826c4ee6ffc1b7aba813f2f11
kinetik <kinetik@orcon.net.nz>
2005-09-01 01:51:39 +0000

    2005-09-01  Matthew Gregan  <kinetik@orcon.net.nz>
    	* monotone.texi: Update 'setup' documentation and tutorial to
    	reflect new usage.  Also update much of the monotone output in the
    	tutorials to reflect the output of more modern versions of
    	monotone.  Correct some minor errors and typos while here.
    	* commands.cc (CMD(setup)): Require database and branch arguments.
    	(ALIAS(import,setup)): Add setup alias.
    	* testsuite.at, tests/*.at: Update 'setup' usage.

--- commands.cc
+++ commands.cc
@@ -3716,20 +3716,22 @@ CMD(log, N_("informative"), N_("[FILE]"),
     }
 }
 
-
 CMD(setup, N_("tree"), N_("DIRECTORY"), N_("setup a new working copy directory"),
     OPT_BRANCH_NAME)
 {
-  string dir;
-
   if (args.size() != 1)
     throw usage(name);
 
-  dir = idx(args,0)();
+  N(!app.branch_name().empty(), F("need --branch argument for setup"));
+  app.db.ensure_open();
+
+  string dir = idx(args,0)();
   app.create_working_copy(dir);
   revision_id null;
   put_revision_id(null);
 }
+
+ALIAS(import, setup);
 
 CMD(automate, N_("automation"),
     N_("interface_version\n"
