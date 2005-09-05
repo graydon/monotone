998f07ce961530c62b78e28b5a98949b8c6a7211
kinetik <kinetik@orcon.net.nz>
2005-09-05 08:50:07 +0000

    2005-09-05  Matthew Gregan  <kinetik@orcon.net.nz>
            * main.cc: Oops, retweak #ifdef to avoid exposing some unused SEH
            handling on MinGW.

--- main.cc
+++ main.cc
@@ -124,7 +124,8 @@ assert_reporting_function(int reportType, char* userMessage, int* retVal)
 }
 #endif
 
-#if defined(MS_STRUCTURED_EXCEPTION_HANDLING) && !defined(__BORLANDC__) && !defined(__MINGW32__)
+#if defined(MS_STRUCTURED_EXCEPTION_HANDLING)
+#if defined !defined(__BORLANDC__) && !defined(__MINGW32__)
 struct
 ms_se_exception 
 {
@@ -201,6 +202,7 @@ report_ms_se_error(unsigned int id)
       report_error("unrecognized exception or signal");
     }
 }
+#endif
 
 #if (defined(__BORLANDC__) && defined(_Windows))
 // this works for Borland but not other Win32 compilers (which trap too many cases)
