adc10065b78ea793918edc2bacd77a13fd943a4f
Timothy Brownawell <tbrownaw@gmail.com>
2005-10-23 18:20:36 +0000

    2005-10-23  Timothy Brownawell  <tbrownaw@gmail.com> 	* std_hooks.lua: new default get_netsync_*_permitted hooks, which 	read permissions setting from $confdir/{read,write}-permissions 	* monotone.texi: document the new default hooks

--- std_hooks.lua
+++ std_hooks.lua
@@ -781,3 +781,67 @@ function external_diff(file_path, data_old, data_new, is_binary, diff_args, rev_
    os.remove (new_file);
 end
 
+function wildcard_match(glob, str)
+   local pat = string.gsub(glob, "([%^%$%(%)%%%.%*%+%-%?])", "%%%1")
+   pat = string.gsub(pat, "%%%*", ".*")
+   pat = string.gsub(pat, "%%%?", ".")
+   pat = string.gsub(pat, "%[!", "%[%^")
+   return string.find(str, pat)
+end
+
+function get_netsync_read_permitted(branch, ident)
+   local permfile = io.open(get_confdir() .. "/read-permissions", "r")
+   if (permfile == nil) then return false end
+   local fallthrough = true
+   local first = true
+   local line = permfile:read()
+   while line ~= nil and (first or fallthrough) do
+      -- find the appropriate section
+      local gotbr
+      while (line ~= nil and not gotbr) do
+         local foo, _, pat = string.find(line, "^%[(.*)%]$")
+         if foo then
+            gotbr = wildcard_match(pat, branch)
+         end
+         line = permfile:read()
+      end
+      -- several [glob] lines immediately following eachother
+      -- share a list of allowed keys
+      while (line ~= nil and string.find(line, "^%[.*%]$")) do
+         line = permfile:read()
+      end
+      -- no matching entries
+      if not gotbr then return false end
+      -- check that section only
+      while (line ~= nil and not string.find(line, "^%[.*%]$")) do
+         local _, _, ln = string.find(line, "%s*([^%s]*)%s*")
+         local _, _, notln = string.find(line, "%s*!%s+([^%s]*)%s*")
+         if ln == "--all--" then return true end
+         if notln == "--all--" then fallthrough = false end
+         if ident ~= nil then
+            if ln == ident then return true end
+            if notln == ident then return false end
+         end
+         line = permfile:read()
+      end
+      first = false
+   end
+   io.close(permfile)
+   return false
+end
+
+function get_netsync_write_permitted(ident)
+   local permfile = io.open(get_confdir() .. "/write-permissions", "r")
+   if (permfile == nil) then
+      return false
+   end
+   local line = permfile:read()
+   while (line ~= nil) do
+      local _, _, ln = string.find(line, "%s*([^%s]*)%s*")
+      if ln == "--all--" then return true end
+      if ln == ident then return true end
+      line = permfile:read()
+   end
+   io.close(permfile)
+   return false
+end
