5a27c718749863c1f594e67a0110d61003d53e97
kinetik <kinetik@orcon.net.nz>
2006-04-08 08:05:42 +0000

    2006-04-08  Matthew Gregan  <kinetik@orcon.net.nz>
    	* monotone.cc (cpp_main): Exclude .exe extension by using
    	fs::basename rather than path.leaf().

--- monotone.cc
+++ monotone.cc
@@ -290,7 +290,7 @@ cpp_main(int argc, char ** argv)
   save_initial_path();
   utf8_argv uv(argc, argv);
 
-  utf8 prog_name(fs::path(uv.argv[0]).leaf());
+  utf8 prog_name(fs::basename(fs::path(uv.argv[0])));
 
   // prepare for arg parsing
 
