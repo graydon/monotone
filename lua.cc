2f8f38e60a391bd8887d214cfeb980951b022a87
Timothy Brownawell <tbrownaw@gmail.com>
2006-04-25 22:02:34 +0000

    2006-04-25  Timothy Brownawell  <tbrownaw@gmail.com> 	* lua.cc: There is now an "extract_classified_str" to go with 	"extract_str", which doesn't log the value of the returned string. 	Use this in hook_get_passphrase. 	* tests/t_no_log_password.at: Test that the passphrase is not logged. 	* testsuite.at: Call it.

--- lua.cc
+++ lua.cc
@@ -204,7 +204,7 @@ Lua
 
   // extractors
 
-  Lua & extract_str(string & str)
+  Lua & extract_str_nolog(string & str)
   {
     if (failed) return *this;
     if (!lua_isstring (st, -1)) 
@@ -213,7 +213,20 @@ Lua
         return *this;
       }
     str = string(lua_tostring(st, -1), lua_strlen(st, -1));
+    return *this;
+  }
+
+  Lua & extract_str(string & str)
+  {
+    extract_str_nolog(str);
     L(FL("lua: extracted string = %s") % str);
+    return *this;
+  }
+
+  Lua & extract_classified_str(string & str)
+  {
+    extract_str_nolog(str);
+    L(FL("lua: extracted string [CLASSIFIED]"));
     return *this;
   }
 
@@ -939,7 +952,7 @@ lua_hooks::hook_get_passphrase(rsa_keypair_id const & k, string & phrase)
     .func("get_passphrase")
     .push_str(k())
     .call(1,1)
-    .extract_str(phrase)
+    .extract_classified_str(phrase)
     .ok();
 }
 
