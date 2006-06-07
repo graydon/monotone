9b50d298935df58979e29d42de0eb82e9948b2e4
Timothy Brownawell <tbrownaw@gmail.com>
2006-06-07 09:02:02 +0000

    decided what test_a_merge_5 should do (pass)

--- tests/test_a_merge_5/__driver__.lua
+++ tests/test_a_merge_5/__driver__.lua
@@ -18,9 +18,17 @@ mtn_setup()
 --             base64< gzip<data> > & dat,
 --             std::string const & table);
 
--- This seems to be a really nasty testcase somehow.  monotone and
--- merge(1) give identical, incorrect output.  diff3(1) reports a
--- conflict.  xxdiff merges cleanly, and, AFAICT, correctly.
+-- This seems to be a really nasty testcase somehow.
+-- merge(1) gives incorrect output.
+-- diff3(1) reports a conflict, and is incorrect.
+-- xxdiff and kdiff3 merge cleanly, and, AFAICT, correctly.
+-- monotone used to be identical to merge(1) here; now it gives a conflict.
+
+-- <njs`> the LCS that diff(1) calculates actually does explain the double-insertion
+-- <njs`> I hate line-merging.
+-- <njs`> anyway, I don't think there actually is a unique right way to merge this :-/
+
+-- Since there's apparently no right way, we accept the conflict as a pass.
 
 getfile("parent")
 getfile("left")
@@ -40,7 +48,7 @@ revert_to(parent)
 copyfile("right", "testfile")
 commit()
 
-check(mtn("--branch=testbranch", "merge"), 0, false, false)
+check(mtn("--branch=testbranch", "merge"), 1, false, false)
 
-check(mtn("update"), 0, false, false)
-xfail_if(true, samefile("testfile", "correct"))
+-- check(mtn("update"), 0, false, false)
+-- check(samefile("testfile", "correct"))
