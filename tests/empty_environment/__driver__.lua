e6227e75abb81605a1f9efb5926679b26178fe87
kinetik <kinetik@orcon.net.nz>
2006-06-21 11:36:51 +0000

    2006-06-21  Matthew Gregan  <kinetik@orcon.net.nz>
    	* tests/empty_environment/__driver__.lua: Fix test on Cygwin--copy
    	essential DLLs into current directory before emptying $PATH.

--- tests/empty_environment/__driver__.lua
+++ tests/empty_environment/__driver__.lua
@@ -11,6 +11,15 @@ if os.getenv("OSTYPE") == "msys" then
   local zlib = getpathof("zlib1", ".dll")
   copy(iconv, "libiconv-2.dll")
   copy(zlib, "zlib1.dll")
+elseif os.getenv("OSTYPE") == "cygwin" then
+  local cygwin = getpathof("cygwin1", ".dll")
+  local iconv = getpathof("cygiconv-2", ".dll")
+  local intl = getpathof("cygintl-3", ".dll")
+  local zlib = getpathof("cygz", ".dll")
+  copy(cygwin, "cygwin1.dll")
+  copy(iconv, "cygiconv-2.dll")
+  copy(intl, "cygintl-3.dll")
+  copy(zlib, "cygz.dll")
 end
 
 check(noenv_mtn("--help"), 2, false, false)
