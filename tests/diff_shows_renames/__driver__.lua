c60f162e3d411bd2cdca7a92db4210bb53936a35
njs <njs@pobox.com>
2006-07-13 21:08:45 +0000

    2006-07-13  Nathaniel Smith  <njs@pobox.com>
    	* tests/restrictions_with_renames_and_adds/__driver__.lua: 
    	* tests/diff_shows_renames/__driver__.lua: Add notes on possible
    	fixes for these bugs.

--- tests/diff_shows_renames/__driver__.lua
+++ tests/diff_shows_renames/__driver__.lua
@@ -11,6 +11,10 @@ check(mtn("diff"), 0, true, false)
 check(qgrep("^--- foo/bar\t", "stdout"))
 check(qgrep("^\\+\\+\\+ foo/newbar\t", "stdout"))
 
+-- The bug here is that the diff code tries to infer renames from the csets
+-- involved.  It should just use the pre- and post-roster to get the pre- and
+-- post-name.
+
 revert_to(root_rev)
 check(mtn("mv", "-e", "foo", "newfoo"), 0, false, false)
 writefile("newfoo/bar", "version 2!\n")
