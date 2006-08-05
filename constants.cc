a006814704b85b2c71d8cfde0ea19a51a6b424f9
njs <njs@pobox.com>
2006-08-05 04:55:52 +0000

    2006-08-04  Nathaniel Smith  <njs@pobox.com>
    	* database.cc: Limit the amount of pending writes we buffer up,
    	from Eric Anderson.  Pluck of 4e99cc37f548b5884d63c48bc486df
    	through	033e5805502c5441ef539ab5cbc1a4, with formatting fixups,
    	factoring out of duplicated code, commented precondition on
    	cancel_pending_write, and removal of comments about the
    	relationship between how often this buffer flushes and how often
    	netsync flushes, since these are logically separate.

--- constants.cc
+++ constants.cc
@@ -44,6 +44,8 @@ namespace constants
 
   size_t const db_roster_cache_sz = 7;
 
+  unsigned long const db_max_pending_writes_bytes = 16 * 1024 * 1024;
+ 
   // size of a line of text in the log buffer, beyond which log lines will be
   // truncated.
   size_t const log_line_sz = 0x300;
--- constants.hh
+++ constants.hh
@@ -55,6 +55,9 @@ namespace constants
   // number of rosters in the database roster cache
   extern size_t const db_roster_cache_sz;
 
+  // maximum number of bytes to be consumed with the pending write cache
+  extern unsigned long const db_max_pending_writes_bytes;
+
   // size of a line of text in the log buffer, beyond which log lines will be
   // truncated.
   extern size_t const log_line_sz;
