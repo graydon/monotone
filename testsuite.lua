4319d88e5015bd1c06316cbeb7c5e04dc952c784
Timothy Brownawell <tbrownaw@gmail.com>
2006-08-14 05:53:02 +0000

    2006-08-14  Timothy Brownawell  <tbrownaw@gmail.com> 	* testsuite.lua: Dump "mtn --full-version" output to the logfile.

--- testsuite.lua
+++ testsuite.lua
@@ -46,6 +46,14 @@ monotone_path = getpathof("mtn")
 if monotone_path == nil then monotone_path = "mtn" end
 set_env("mtn", monotone_path)
 
+writefile_q("in", nil)
+prepare_redirect("in", "out", "err")
+execute(monotone_path, "--full-version")
+logfile:write(readfile_q("out"))
+unlogged_remove("in")
+unlogged_remove("out")
+unlogged_remove("err")
+
 -- NLS nuisances.
 for _,name in pairs({  "LANG",
                        "LANGUAGE",
