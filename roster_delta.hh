c4b04fe9ec16a78cf36022df6d7b90606002477b
njs <njs@pobox.com>
2006-08-15 07:54:34 +0000

    more unit tests

--- roster_delta.hh
+++ roster_delta.hh
@@ -19,6 +19,9 @@ apply_roster_delta(roster_delta const & del,
 
 #ifdef BUILD_UNIT_TESTS
 
+// instead of having elaborate tests here, we just export a function, and then
+// let all the other code that already generates every strange and weird test
+// case there is feed us our tests
 void test_roster_delta_on(roster_t const & a, marking_map const & a_marking,
                           roster_t const & b, marking_map const & b_marking);
 
