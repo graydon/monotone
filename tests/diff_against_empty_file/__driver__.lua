4bb648668d9372c605e11fd12250f1d871d97b14
jon <jon@siliconcircus.com>
2006-08-23 17:49:48 +0000

    2006-08-23  Jon Bright  <jon@siliconcircus.com>
    	* tests/diff_against_empty_file/__driver__.lua: Canonicalize the
    	output before checking.

--- tests/diff_against_empty_file/__driver__.lua
+++ tests/diff_against_empty_file/__driver__.lua
@@ -8,6 +8,7 @@ commit("foo", "foo")
 rename("foo", "foo.away")
 writefile("foo")
 check(mtn("diff", "foo"), 0, true)
+canonicalize("stdout")
 rename("stdout", "monodiff")
 
 check(samefile("monodiff", "expected.diff"))
