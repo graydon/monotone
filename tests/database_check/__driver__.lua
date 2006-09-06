b393039c2bc6929b2233ac5ad7e357b2fd0cee93
njs <njs@pobox.com>
2006-09-06 23:29:33 +0000

    make database_check test pass again

--- tests/database_check/__driver__.lua
+++ tests/database_check/__driver__.lua
@@ -67,7 +67,7 @@ writefile("_MTN/revision", revs[3])
 dbex("delete from files where id='%s'", files[3])
 
 check(mtn("db", "check", "--ticker=dot"), 1, false, true)
-check(qgrep('1 unreferenced file', "stderr"))
+check(qgrep('2 unreferenced file', "stderr"))
 check(qgrep('1 unreferenced roster', "stderr"))
 check(qgrep('2 missing files', "stderr"))
 
