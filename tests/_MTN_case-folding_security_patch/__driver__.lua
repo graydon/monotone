2e9c7d87ad68cb1b9ce461d4110000fe4cffd12b
njs <njs@pobox.com>
2006-09-07 00:56:59 +0000

    beef up the _mTn test a little, to compensate for weakening it

--- tests/_MTN_case-folding_security_patch/__driver__.lua
+++ tests/_MTN_case-folding_security_patch/__driver__.lua
@@ -30,6 +30,24 @@ end
 check(mtn("ls", "known"), 0)
 for _,i in pairs(names) do remove(i) end
 
+-- just to make sure, check that it's not only add that fails, if it somehow
+-- sneaks into an internal format then that fails too
+remove("_MTN")
+check(mtn("--branch=testbranch", "setup", "."))
+mkdir("_mTn")
+writefile("_MTN/revision",
+             'format_version "1"\n'
+          .. '\n'
+          .. 'new_manifest []\n'
+          .. '\n'
+          .. 'old_revision []\n'
+          .. '\n'
+          .. 'add_dir ""\n'
+          .. '\n'
+          .. 'add_dir "_mTn"\n')
+check(mtn("status"), 3, false, false)
+check(mtn("commit", "-m", "blah"), 3, false, false)
+
 -- assert trips if we have a db that already has a file with this sort
 -- of name in it.  it would be better to test that checkout or pull fail, but
 -- it is too difficult to regenerate this database every time things change,
