75b75e54df768bc5b37fa5060cf1ae306e0b54a7
njs <njs@pobox.com>
2006-09-12 09:02:00 +0000

    2006-09-12  Nathaniel Smith  <njs@pobox.com>
    	* tests/ws_ops_with_wrong_node_type: Add two failing test cases,
    	from bug report from Albert Ramstedt.

---
+++ tests/ws_ops_with_wrong_node_type/__driver__.lua
@@ -0,0 +1,20 @@
+mtn_setup()
+
+addfile("file", "asdf")
+mkdir("dir")
+check(mtn("add", "dir"), 0, false, false)
+commit()
+
+check(mtn("automate", "get_revision"), 0, true, false)
+rename("stdout", "orig_rev")
+
+-- fail to move a dir under a file
+xfail(mtn("rename", "dir", "file/subdir"), 1, false, false)
+check(mtn("automate", "get_revision"), 0, true, false)
+check(samefile("stdout", "orig_rev"))
+
+-- running a recursive add what's supposed to be a file, but is actually a
+-- dir...
+mkdir("dir2")
+check(mtn("rename", "file", "dir2"), 0, false, false)
+xfail(mtn("add", "dir2"), 0, false, false)
