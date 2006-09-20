c803f2e83784dd88e7c8ceb3aa66bfb6b1dd95aa
njs <njs@pobox.com>
2006-09-20 23:44:26 +0000

    2006-09-20  Nathaniel Smith  <njs@pobox.com>
    	* roster.hh: *sigh* Previous changes were just dumb.  Remove
    	them again.

--- roster.hh
+++ roster.hh
@@ -52,9 +52,6 @@ typedef std::map<path_component, node_t> dir_map;
 typedef std::map<node_id, node_t> node_map;
 
 template <> void dump(full_attr_map_t const & val, std::string & out);
-template <> void dump(std::set<revision_id> const & revids, std::string & out);
-template <> void dump(marking_t const & marking, std::string & out);
-template <> void dump(marking_map const & markings, std::string & out);
 
 
 struct node
