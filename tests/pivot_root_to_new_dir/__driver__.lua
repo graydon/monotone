3824ea6f74a210752b9139fd9613a440a7589a14
mtn-dev <mtn-dev@zackw.users.panix.com>
2006-09-22 19:46:03 +0000

    * tests/pivot_root_to_new_dir: No longer expected to fail. 	* work.cc (workspace::perform_pivot_root): Partially revert 	revision 7e648a47fdd80f004a529ef5ddba9428f59f673d: apply the 	renames to the current roster and then calculate the complete set 	of changes, instead of just blindly writing the changeset to the 	workspace revision.

--- tests/pivot_root_to_new_dir/__driver__.lua
+++ tests/pivot_root_to_new_dir/__driver__.lua
@@ -1,9 +1,10 @@
 mtn_setup()
 
--- pivot_root to a directory that hasn't been commited yet fails in 0.30
+-- pivot_root to a directory that hasn't been commited yet should work
 
 mkdir("workspace")
-check(indir("workspace", mtn("setup", ".", "-b", "testbranch")), 0, false, false)
+check(indir("workspace", mtn("setup", ".", "-b", "testbranch")),
+      0, false, false)
 
 writefile("workspace/file1", "blah blah")
 check(indir("workspace", mtn("add", ".")), 0, false, false)
@@ -13,7 +14,8 @@ check(indir("workspace", mtn("commit", "-m", "foo")), 0, false, false)
 mkdir("workspace/new_root")
 check(indir("workspace", mtn("add", "new_root")), 0, false, false)
 
-xfail(indir("workspace", mtn("pivot_root", "-e", "new_root", "Attic")), 0, false, false)
+check(indir("workspace", mtn("pivot_root", "-e", "new_root", "Attic")),
+      0, false, false)
 
 check(isdir("workspace/_MTN"))
 check(isdir("workspace/Attic"))
