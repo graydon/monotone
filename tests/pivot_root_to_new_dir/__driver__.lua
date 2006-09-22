d1e784136e475cb58aaa526aab030b6a5ad92a39
Matt Johnston <matt@ucc.asn.au>
2006-09-22 15:06:41 +0000

    2006-09-22	Matt Johnston  <matt@ucc.asn.au> 	* tests/pivot_root_to_new_dir/: new test for pivot_root into 	a directory that hasn't been committed yet (xfailed, 0.30 regression)

---
+++ tests/pivot_root_to_new_dir/__driver__.lua
@@ -0,0 +1,23 @@
+mtn_setup()
+
+-- pivot_root to a directory that hasn't been commited yet fails in 0.30
+
+mkdir("workspace")
+check(indir("workspace", mtn("setup", ".", "-b", "testbranch")), 0, false, false)
+
+writefile("workspace/file1", "blah blah")
+check(indir("workspace", mtn("add", ".")), 0, false, false)
+
+check(indir("workspace", mtn("commit", "-m", "foo")), 0, false, false)
+
+mkdir("workspace/new_root")
+check(indir("workspace", mtn("add", "new_root")), 0, false, false)
+
+xfail(indir("workspace", mtn("pivot_root", "-e", "new_root", "Attic")), 0, false, false)
+
+check(isdir("workspace/_MTN"))
+check(isdir("workspace/Attic"))
+check(exists("workspace/Attic/file1"))
+check(not exists("workspace/new_root"))
+
+check(indir("workspace", mtn("commit", "-m", "pivotted")), 0, false, false)
