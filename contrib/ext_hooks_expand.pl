82172fe174aedba1ead9d30ec6ba5709f6d42e3e
richard <richard@levitte.org>
2006-11-16 19:52:44 +0000

    2006-11-16  Richard Levitte  <richard@levitte.org>
    	* contrib/ext_hooks.lua.in, contrib/ext_hooks_expand.pl: New
    	files that implement a system that allows easy addition of hooks
    	implementations without having to edit any file. You need to
    	run the perl script contrib/ext_hooks_expand.pl to produce the
    	file contrib/ext_hooks.lua, using contrib/ext_hooks.lua.in as
    	a template and the hook function definitions found in
    	monotone.texi.
    	* README: Add a small blurb about the above files.

---
+++ contrib/ext_hooks_expand.pl
@@ -0,0 +1,96 @@
+#! /usr/bin/perl
+
+use warnings;
+use strict;
+use File::Basename;
+use File::Spec;
+
+my $here = dirname($0);
+my $there = File::Spec->catdir($here,File::Spec->updir());
+my $monotone_texi = File::Spec->catfile($there, "monotone.texi");
+my $ext_hooks_lua_in = File::Spec->catfile($here, "ext_hooks.lua.in");
+my $ext_hooks_lua = File::Spec->catfile($here, "ext_hooks.lua");
+
+# Format:  $name => $arglist
+my %hooks = ();
+
+open INPUT, $monotone_texi ||
+    die "Couldn't open $monotone_texi for reading: $!\n";
+
+# First, read until we find the Hooks section
+while(<INPUT>) {
+    chomp;
+    last if (/^\s*\@section\s+Hooks\s*$/);
+}
+
+# Now, parse all the hooks docs until we find the Additional Lua Functions
+# section
+while(<INPUT>) {
+    chomp;
+    last if (/^\s*\@section\s+Additional\s+Lua\s+Functions\s*$/);
+    if (/^\s*\@item\s+(\w+)\s*\((\@var\{\w+\}(,\s*\@var\{\w+\})*)?\)\s*$/) {
+	my $name = $1;
+	my $arglist = "";
+	if (defined $2) {
+	    $arglist = join(", ",
+			    map {
+				my $x = $_;
+				$x =~ s/^\s*\@var\{(\w+)\}\s*$/$1/;
+				$x;
+			    } split(/,\s*/, $2));
+	}
+	$hooks{$name} = $arglist;
+    }
+}
+
+close INPUT;
+
+foreach my $n (sort keys %hooks) {
+    print $n," => \"",$hooks{$n},"\"\n";
+}
+
+open INPUT, $ext_hooks_lua_in ||
+    die "Couldn't open $ext_hooks_lua_in for reading: $!\n";
+open OUTPUT, ">$ext_hooks_lua" ||
+    die "Couldn't open $ext_hooks_lua for writing: $!\n";
+
+# Format : $name => [ list of strings ]
+my %expansions = ( HOOKS_LIST => [],
+		   HOOKS_FUNCTIONS => []);
+
+foreach my $n (sort keys %hooks) {
+    push(@{$expansions{HOOKS_LIST}}, $n);
+    push(@{$expansions{HOOKS_FUNCTIONS}},
+"function $n ($hooks{$n})
+   local i
+   local fn
+   res_$n = nil
+   for i,fn in pairs(mtn_luahooks[\"$n\"]) do
+      res = fn($hooks{$n})
+      if res ~= nil then res_$n = res end
+   end
+   return res_$n
+end"
+	 );
+}
+
+while(<INPUT>) {
+    chomp;
+    if (/^(.*)\@\@(\w+)\@\@(.*)$/) {
+	my $prefix = $1;
+	my $listname = $2;
+	my $postfix = $3;
+	if (defined $expansions{$listname}) {
+	    foreach my $item (@{$expansions{$listname}}) {
+		print OUTPUT $prefix,$item,$postfix,"\n";
+	    }
+	} else {
+	    print OUTPUT $prefix,$listname,$postfix,"\n";
+	}
+    } else {
+	print OUTPUT $_,"\n";
+    }
+}
+
+close OUTPUT;
+close INPUT;
