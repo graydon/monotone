9c29b5543d8f720ece4d30120346397711c70b5d
mtn-dev <mtn-dev@zackw.users.panix.com>
2007-01-12 19:19:07 +0000

    * tests/db_migrate_on_bad_schema: Rename to schema_migration_bad_schema. 	Rewrite to test for specific error messages corresponding to the 	five-way distinction made by check_sql_schema. 	* tests/migrate_with_rosterify: Rename to 	schema_migration_with_rosterify. 	* testsuite.lua: Update.

--- tests/db_migrate_on_bad_schema/__driver__.lua
+++
@@ -1,8 +0,0 @@
-
-mtn_setup()
-
-check(mtn("db", "migrate"), 0, false, false)
-
-check(mtn("db", "execute", 'CREATE TABLE foo (bar primary key, baz not null)'), 0, false, false)
-
-check(mtn("db", "migrate"), 1, false, false)
