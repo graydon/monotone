54c6c385f4de8332fb765202c0bc68df16d3a0db
dan <dan@geek.com.au>
2007-02-08 07:41:25 +0000

    add a test for another revert I() I hit today

---
+++ tests/revert_the_middle_of_a_sequence/__driver__.lua
@@ -0,0 +1,20 @@
+mtn_setup()
+
+addfile("file1", "original file1")
+commit()
+
+-- setup 
+check(mtn("rename", "file1", "file2"), 0, true, true)
+addfile("file1", "new file1")
+
+-- first check; revert the new file which will end up reverting both changes
+-- XXX is this really desired behaviour?
+check(mtn("revert", "file1"), 0, true, true)
+-- check results
+
+-- setup 
+check(mtn("rename", "file1", "file3"), 0, true, true)
+addfile("file1", "new file1")
+
+-- second check; revert the renamed file which will.. uh.. trip an I()
+xfail(check(mtn("revert", "file3"), 3, true, true))
