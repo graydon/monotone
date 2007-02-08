e02dfe29ca25256617bceabb729c2e1a0c7e584b
mtn-dev <mtn-dev@zackw.users.panix.com>
2007-02-08 05:43:03 +0000

    * cmd_ws_commit.cc (attr): Work in a two-parent workspace. 	* tests/two_parent_workspace_attrs: New test. 	* tests/two_parent_workspace_pluck: Make sure we can commit the 	result of our manipulations.

--- tests/two_parent_workspace_attrs/__driver__.lua
+++ tests/two_parent_workspace_attrs/__driver__.lua
@@ -1,16 +1,13 @@
 mtn_setup()
 
 addfile("testfile", "ancestor\nancestor")
-addfile("pluckfile", "quack quack")
+addfile("attrfile", "this file has attributes")
 commit()
 anc = base_revision()
 
-writefile("pluckfile", "brawwk brawwk")
-commit()
-pluckrev = base_revision()
-
-revert_to(anc)
 writefile("testfile", "left\nancestor")
+check(mtn("attr", "set", "attrfile", "my other car", "made of meat"),
+      0, nil, nil)
 commit()
 left = base_revision()
 
@@ -23,10 +20,11 @@ check(mtn("merge_into_workspace", left), 0, false, false)
 check(qgrep("left", "testfile"))
 check(qgrep("right", "testfile"))
 check(not qgrep("ancestor", "testfile"))
-check(qgrep("quack quack", "pluckfile"))
+check(mtn("attr", "get", "attrfile", "my other car"),
+      0, "attrfile : my other car=made of meat\n", nil)
+check(mtn("attr", "set", "testfile", "astral cupcake", "pink frosted"),
+      0, nil, nil)
+check(mtn("attr", "drop", "attrfile", "my other car"),
+      0, nil, nil)
 
-check(mtn("pluck", "-r", pluckrev), 0, false, false)
-check(qgrep("brawwk brawwk", "pluckfile"))
-
-remove("_MTN/log") -- or commit() will get a bogus failure
 commit()
