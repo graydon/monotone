db7d976a57fc842a4c698c4aed481d4e08e47888
papercrane <papercrane@reversefold.com>
2007-02-08 02:43:33 +0000

    Export monotone keys as PKCS8 PEM files for use with ssh-agent - export get_passphrase and get_private_key in keys.hh - made get_passphrase no longer static as it was causing compile problems

--- cmd_agent.cc
+++ cmd_agent.cc
@@ -1,11 +1,25 @@
 #include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+#include <iostream>
+#include <fstream>
 
 #include "cmd.hh"
+#include "keys.hh"
 #include "ssh_agent.hh"
+#include "botan/rsa.h"
+#include "botan/base64.h"
+#include "botan/pipe.h"
 
+using std::cout;
 using std::string;
 using std::vector;
+using std::fstream;
 using boost::scoped_ptr;
+using boost::shared_ptr;
+using Botan::RSA_PublicKey;
+using Botan::RSA_PrivateKey;
+using Botan::Base64_Encoder;
+using Botan::Pipe;
 
 static void
 agent_list(string const & name, app_state & app, vector<utf8> const & args)
@@ -18,8 +32,41 @@ agent_list(string const & name, app_state & app, vector<utf8> const & args)
   a->get_keys();
 }
 
+static void
+agent_export(string const & name, app_state & app, vector<utf8> const & args)
+{
+  if (args.size() != 0 && args.size() != 1)
+    throw usage(name);
+
+  vector<rsa_keypair_id> keys;
+  keypair key;
+  if (args.size() == 0)
+    app.keys.get_keys(keys);
+  else
+    app.keys.get_key_ids(idx(args,0)(), keys);
+  for (vector<rsa_keypair_id>::const_iterator
+         i = keys.begin(); i != keys.end(); ++i) {
+    app.keys.get_key_pair(*i, key);
+    //cout << key.priv << "\n";
+    shared_ptr<RSA_PrivateKey> priv = get_private_key(app.lua, *i, key.priv);
+
+    utf8 new_phrase;
+    get_passphrase(app.lua, *i, new_phrase, true, true, "enter new passphrase");
+    Pipe p;
+    p.start_msg();
+    Botan::PKCS8::encrypt_key(*priv, p, new_phrase(),
+                              "PBE-PKCS5v20(SHA-1,TripleDES/CBC)");
+    string decoded_key = p.read_all_as_string();
+    fstream fout;
+    fout.open("id_monotone", fstream::out | fstream::trunc);
+    fout << decoded_key;
+    fout.close();
+  }
+}
+
 CMD(agent, N_("informative"),
-    N_("list"),
+    N_("list\n"
+       "export"),
     N_("interact with the agent"),
     options::opts::depth | options::opts::exclude)
 {
@@ -31,6 +78,8 @@ CMD(agent, N_("informative"),
   vector<utf8> removed (i, args.end());
   if (idx(args, 0)() == "list")
     agent_list(name, app, removed);
+  else if (idx(args, 0)() == "export")
+    agent_export(name, app, removed);
   else
     throw usage(name);
 }
