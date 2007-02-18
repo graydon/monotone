28fcc7a4e4341ef0691da18c2988ff53611b3846
lapo <lapo@lapo.it>
2007-02-18 17:52:53 +0000

    * asciik.cc: it's a graph, not a tree. 	* po/it.po: merged and updated (49 to go).

--- asciik.cc
+++ asciik.cc
@@ -379,7 +379,7 @@ asciik::print(revision_id const & rev,
 }
 
 CMD(asciik, N_("debug"), N_("SELECTOR"),
-  N_("prints an ASCII-art tree representation"), options::opts::none)
+  N_("prints an ASCII representation of the graph"), options::opts::none)
 {
   N(args.size() == 1,
     F("wrong argument count"));
