4214f2837a4ae11a3a01fc7ee6bc9affcdd2af1a
kinetik <kinetik@orcon.net.nz>
2007-02-20 17:16:44 +0000

    * {unix,win32}/{cputime,fs,get_system_flavour,inodeprint}.cc: Add 	  magic editor variables block. 	* {unix,win32}/{make_io_binary,os_strerror,process}.cc: Ditto. 	* {unix,win32}/{read_password,terminal}.cc: Ditto. 	* do-editor-vars.sh: Update to check unix/ and win32/ subdirs.

--- unix/make_io_binary.cc
+++ unix/make_io_binary.cc
@@ -3,3 +3,10 @@ void make_io_binary()
 {
 }
 
+// Local Variables:
+// mode: C++
+// fill-column: 76
+// c-file-style: "gnu"
+// indent-tabs-mode: nil
+// End:
+// vim: et:sw=2:sts=2:ts=2:cino=>2s,{s,\:s,+s,t0,g0,^-2,e-2,n-2,p2s,(0,=s:
