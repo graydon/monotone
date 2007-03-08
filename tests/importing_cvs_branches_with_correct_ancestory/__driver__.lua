5b6083de8e49c7c63c91e157d220c807ecbd4576
markus <markus@bluegap.ch>
2007-03-08 09:04:06 +0000

    * tests/cvs_import_with_file_added_on_a_branch/*,    tests/importing_a_cvs_file_with_one_version/*,    tests/tests/importing_cvs_branches_with_correct_ancestory/*,    tests/importing_cvs_branches_with_correct_ancestory/*,    tests/importing_cvs_files/*,    tests/importing_cvs_files_with_identical_logs/*,    tests/importing_cvs_with_vendor_imports_and_branches/*: removed the    remaining dependencies on cvs.lua, which in turn depends on cvs being    installed to run these tests. To do that, I've added the complete    cvs-repository, as it is used for the tests. Plus a makerepos.sh has    been added, solely to show how the repository has been created. It's    not used during the unit tests.  * tests/common/cvs.lua: removed, since it's no longer necessary.

--- tests/importing_cvs_branches_with_correct_ancestory/__driver__.lua
+++ tests/importing_cvs_branches_with_correct_ancestory/__driver__.lua
@@ -1,68 +1,34 @@
 
-include("common/cvs.lua")
 mtn_setup()
 
-writefile("file1.0", "version 0 of test file1")
-writefile("file1.1", "version 1 of test file1")
-writefile("file1.2", "version 2 of test file1")
-writefile("file2.0", "version 0 of test file2")
-writefile("file2.1", "version 1 of test file2")
+
+-- See makerepo.sh on how this repository was created.
+check(get("cvs-repository"))
+
+writefile("file1.0", "version 0 of test file1\n")
+writefile("file1.1", "version 1 of test file1\n")
+writefile("file1.2", "version 2 of test file1\n")
+writefile("file2.0", "version 0 of test file2\n")
+writefile("file2.1", "version 1 of test file2\n")
 
 writefile("changelog.0", "first changelog entry\n")
-
-writefile("changelog.1", "second changelog\n\n"..readfile("changelog.0"))
-writefile("changelog.2", "third changelog -not on branch-\n\n"..readfile("changelog.1"))
-writefile("changelog.3", "third changelog -on branch-\n\n"..readfile("changelog.1"))
+writefile("changelog.1", readfile("changelog.0").."second changelog\n")
+writefile("changelog.2", readfile("changelog.1").."third changelog -not on branch-\n")
+writefile("changelog.3", readfile("changelog.1").."third changelog -on branch-\n")
 
 F1SHA0=sha1("file1.0")
 F1SHA1=sha1("file1.1")
 F1SHA2=sha1("file1.2")
 F2SHA0=sha1("file2.0")
-T2SHA1=sha1("file2.1")
+F2SHA1=sha1("file2.1")
 CSHA0=sha1("changelog.0")
 CSHA1=sha1("changelog.1")
 CSHA2=sha1("changelog.2")
 CSHA3=sha1("changelog.3")
 
--- build the cvs repository
-
-cvs_setup()
-
--- checkout the empty repository and commit some files
-
-check(cvs("co", "."), 0, false, false)
-mkdir("testdir")
-writefile("testdir/file1", readfile("file1.0"))
-writefile("testdir/file2", readfile("file2.0"))
-writefile("testdir/changelog", readfile("changelog.0"))
-check(cvs("add", "testdir"), 0, false, false)
-check(cvs("add", "testdir/file1"), 0, false, false)
-check(cvs("add", "testdir/file2"), 0, false, false)
-check(cvs("add", "testdir/changelog"), 0, false, false)
-check(cvs("commit", "-m", 'initial import', "testdir/file1", "testdir/file2", "testdir/changelog"), 0, false, false)
-
--- commit first changes
-writefile("testdir/file1", readfile("file1.1"))
-writefile("testdir/changelog", readfile("changelog.1"))
-check(cvs("commit", "-m", 'first commit', "testdir/file1", "testdir/changelog"), 0, false, false)
-
--- now we create a branch
-check(indir("testdir", cvs("tag", "-b", "branched")), 0, false, false)
-check(indir("testdir", cvs("up", "-r", "branched")), 0, false, false)
-
--- alter the files on the branch
-writefile("testdir/file2", readfile("file2.1"))
-writefile("testdir/changelog", readfile("changelog.3"))
-check(cvs("commit", "-m", 'commit on branch', "testdir/file2", "testdir/changelog"), 0, false, false)
-
--- and create some mainline changes after the branch
-check(cvs("up", "-A"), 0, false, false)
-writefile("testdir/file1", readfile("file1.2"))
-writefile("testdir/changelog", readfile("changelog.2"))
-check(cvs("commit", "-m", 'commit on mainline after branch', "testdir/file1", "testdir/changelog"), 0, false, false)
 
 -- import into monotone and check presence of files
-check(mtn("--branch=test", "cvs_import", cvsroot.."/testdir"), 0, false, false)
+check(mtn("--branch=test", "cvs_import", "cvs-repository/test"), 0, false, false)
 
 -- check if all branches were imported
 check(mtn("list", "branches"), 0, true, false)
