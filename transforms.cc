15faf4d76dab7a4bc29c63fc15c015d696f9c449
patrick <patrick@georgi-clan.de>
2007-03-13 12:15:59 +0000

    * transforms.cc: error_in_transform is now static inline to make sunpro 	  happy

--- transforms.cc
+++ transforms.cc
@@ -60,9 +60,9 @@ using boost::scoped_array;
 // paradigm "must" be used. this program is intended for source code
 // control and I make no bones about it.
 
-NORETURN(static void error_in_transform(Botan::Exception & e));
+NORETURN(static inline void error_in_transform(Botan::Exception & e));
 
-static void
+static inline void
 error_in_transform(Botan::Exception & e)
 {
   // why do people make up their own out-of-memory exceptions?
