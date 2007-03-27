54c8003619172926a39639a031f92c6620b0ed32
Matt Johnston <matt@ucc.asn.au>
2007-03-27 12:14:44 +0000

    * roster.cc (make_roster_for_merge,mark_merged_node): pass uncommon 	ancestor sets as const references rather than copies.

--- roster.cc
+++ roster.cc
@@ -1443,10 +1443,10 @@ namespace
 
   void
   mark_merged_node(marking_t const & left_marking,
-                   set<revision_id> left_uncommon_ancestors,
+                   set<revision_id> const & left_uncommon_ancestors,
                    node_t ln,
                    marking_t const & right_marking,
-                   set<revision_id> right_uncommon_ancestors,
+                   set<revision_id> const & right_uncommon_ancestors,
                    node_t rn,
                    revision_id const & new_rid,
                    node_t n,
@@ -1711,13 +1711,13 @@ namespace {
                         roster_t const & left_roster,
                         marking_map const & left_markings,
                         cset const & left_cs,
-                        set<revision_id> left_uncommon_ancestors,
+                        set<revision_id> const & left_uncommon_ancestors,
 
                         revision_id const & right_rid,
                         roster_t const & right_roster,
                         marking_map const & right_markings,
                         cset const & right_cs,
-                        set<revision_id> right_uncommon_ancestors,
+                        set<revision_id> const & right_uncommon_ancestors,
 
                         revision_id const & new_rid,
                         roster_t & new_roster,
