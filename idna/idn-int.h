ddcca9f144c7c8cab3dad12b5356d19a701a0f74
Richard Levitte <richard@levitte.org>
2007-07-03 18:37:50 +0000

    * idna/idn-int.h: Adapt to the new vocabulary for exact size integers

--- idna/idn-int.h
+++ idna/idn-int.h
@@ -1,1 +1,10 @@
-#include "mt-stdint.h"
+#include "config.h"
+
+#define uint8_t TYPE_U8
+#define uint16_t TYPE_U16
+#define uint32_t TYPE_U32
+#define uint64_t TYPE_U64
+#define int8_t TYPE_S8
+#define int16_t TYPE_S16
+#define int32_t TYPE_S32
+#define int64_t TYPE_S64
