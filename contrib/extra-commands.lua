45ab1fc08d1816736224ed8f3f20fad688819934
willu <willu@cse.unsw.edu.au>
2007-07-08 03:45:11 +0000

    Update the user commands to be more useful. Add a description of the user commands file to the README in contrib.

--- contrib/extra-commands.lua
+++ contrib/extra-commands.lua
@@ -3,19 +3,17 @@
 alias_command("annotate", "blame")
 alias_command("annotate", "praise")
 
-function pmup(...)
+function pup(...)
     mtn_automate("get_option", "branch")	-- make sure we have a valid workspace
     mtn_automate("pull")
-    mtn_automate("merge")
     mtn_automate("update")
 end
 
-register_command("pup", "Pull, merge and update a workspace",
+register_command("pup", "Pull and update a workspace",
       "This command approximates the update command of a centralised revision control system.  " ..
-      "It first contacts the server to gather new revisions, then merges multiple local heads " ..
-      "(if any), and then it updates the workspace.", "pmup")
+      "It first contacts the server to gather new revisions and then it updates the workspace.", "pup")
 
-function cpm(...)
+function cpp(...)
     mtn_automate("get_option", "branch")	-- make sure we have a valid workspace
     mtn_automate("commit")
     mtn_automate("pull")
@@ -25,17 +23,27 @@ function cpm(...)
     if words == 1 then
         mtn_automate("push")
     else
-        mtn_automate("merge")
-        print("Workspace contents will not be pushed to the server.")
-        print("Please check that merge was successful then push changes.")
+        print("There are multiple heads in your current branch.")
+        print("You should run 'mtn mup' to merge the heads and update.")
+        print("After you have verified the merge works, run 'mtn cpp'")
+        print("again to commit and push the changes.")
     end
 end
 
-register_command("cpm", "Commit, pull, merge and push a workspace",
+register_command("cpp", "Commit, pull and push a workspace",
       "This command approximates the commit command of a centralised revision control system.  " ..
       "It first commits your work to the local repository, then contacts the server to gather " ..
       "new revisions.  If there is a single head at this point, then the local changes are pushed " ..
-      "to the server.  If there are multiple heads then they are merged, and the user is asked " ..
-      "to check things still work before pushing the changes.", "cpm")
+      "to the server.", "cpp")
+
+function mup(...)
+    mtn_automate("get_option", "branch")	-- make sure we have a valid workspace
+    mtn_automate("merge")
+    mtn_automate("update")
+end
+
+register_command("mup", "Merge and update a workspace",
+      "This command merges multiple heads of a branch, and then updates the current workspace" ..
+      "to the resulting revision.", "mup")
 
 
