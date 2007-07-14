068f87f87f6114c005bcde97c7128e26c7b19bf3
kfh <kfh@mqsoftware.com>
2007-07-14 17:45:08 +0000

    fixed runtests error handling

--- visualc/runtests.bat
+++ visualc/runtests.bat
@@ -5,21 +5,26 @@ set PATH=%CD%\visualc\Release;%PATH%
 unit_tests.exe
 if %ERRORLEVEL%=="0" goto tester-testsuite.lua
 @echo unit-tests failed, skipping remaining tests.
-goto end
+goto error
 
 :tester-testsuite.lua
 tester.exe tester-testsuite.lua
 if NOT %ERRORLEVEL%=="0" goto testsuite.lua
 @echo tester-testsuite.lua failed, skipping remaining tests.
-goto end
+goto error
 
 :testsuite.lua
 tester.exe testsuite.lua
 if NOT %ERRORLEVEL%=="0" goto end
 @echo testsuite.lua failed.
+goto error
+
+
+
 goto end
-
-
+:error
+@rem return an error code
+exit /B 1
 
 
 
