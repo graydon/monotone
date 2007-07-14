2d6768643b0ebdf813fe43a499b37f65461507ba
kfh <kfh@mqsoftware.com>
2007-07-14 17:02:43 +0000

    change for proper errorlevel detection

--- visualc/runtests.bat
+++ visualc/runtests.bat
@@ -9,13 +9,13 @@ goto end
 
 :tester-testsuite.lua
 tester.exe tester-testsuite.lua
-if %ERRORLEVEL%=="0" goto testsuite.lua
+if NOT %ERRORLEVEL%=="0" goto testsuite.lua
 @echo tester-testsuite.lua failed, skipping remaining tests.
 goto end
 
 :testsuite.lua
 tester.exe testsuite.lua
-if %ERRORLEVEL%=="0" goto end
+if NOT %ERRORLEVEL%=="0" goto end
 @echo testsuite.lua failed.
 goto end
 
