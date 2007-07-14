f54d4da1cc803212a2b3b6533bc154a5447ac12b
kfh <kfh@mqsoftware.com>
2007-07-14 16:53:29 +0000

    make runtests detect test failures and abort

--- visualc/runtests.bat
+++ visualc/runtests.bat
@@ -1,7 +1,26 @@
 SETLOCAL
 
 set PATH=%CD%\visualc\Release;%PATH%
-unit_tests.exe
-tester.exe tester-testsuite.log
-tester.exe testsuite.lua
 
+unit_tests.exe
+if %ERRORLEVEL%=="0" goto tester-testsuite.lua
+@echo unit-tests failed, skipping remaining tests.
+goto end
+
+:tester-testsuite.lua
+tester.exe tester-testsuite.lua
+if %ERRORLEVEL%=="0" goto testsuite.lua
+@echo tester-testsuite.lua failed, skipping remaining tests.
+goto end
+
+:testsuite.lua
+tester.exe testsuite.lua
+if %ERRORLEVEL%=="0" goto end
+@echo testsuite.lua failed.
+goto end
+
+
+
+
+
+:end
