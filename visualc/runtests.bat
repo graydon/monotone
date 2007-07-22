5ca2ae84720970ee548170dc9ef0671f9ad63291
Richard Levitte <richard@levitte.org>
2007-07-22 08:12:15 +0000

    * runtests.bat: Have Windows do all tests and keep a count of failed ones.

--- visualc/runtests.bat
+++ visualc/runtests.bat
@@ -2,30 +2,38 @@ SETLOCAL
 
 set PATH=%CD%\visualc\Release;%PATH%
 
+set fail_count=0
+
 unit_tests.exe
 if %ERRORLEVEL%=="0" goto tester-testsuite.lua
-@echo unit-tests failed, skipping remaining tests.
-goto error
+@echo unit-tests failed.
+set fail_count=1
 
 :tester-testsuite.lua
 tester.exe tester-testsuite.lua
 if NOT %ERRORLEVEL%=="0" goto testsuite.lua
-@echo tester-testsuite.lua failed, skipping remaining tests.
-goto error
+@echo tester-testsuite.lua failed.
+if %fail_count%=="1" set fail_count=2
+if %fail_count%=="0" set fail_count=1
 
 :testsuite.lua
 tester.exe testsuite.lua
-if NOT %ERRORLEVEL%=="0" goto end
+if NOT %ERRORLEVEL%=="0" goto nomore
 @echo testsuite.lua failed.
-goto error
+if %fail_count%=="2" set fail_count=3
+if %fail_count%=="1" set fail_count=2
+if %fail_count%=="0" set fail_count=1
 
+:nomore
+if %fail_count%=="0" goto end
 
-
-goto end
-:error
 @rem return an error code
+@echo "==============================="
+@echo %fail_count of 3 tests failed.
+@echo "==============================="
 exit /B 1
 
-
-
 :end
+@echo "==============================="
+@echo All tests succeeded!
+@echo "==============================="
