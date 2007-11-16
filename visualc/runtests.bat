c18e51794f503767774b284355b9b5b64d320c18
kfh <kfh@mqsoftware.com>
2007-11-16 19:42:50 +0000

    cleaned up windows runtests.bat.  comparisons were wrong and tester.exe seems to fail when launched from the path, so specify relative location

--- visualc/runtests.bat
+++ visualc/runtests.bat
@@ -1,33 +1,32 @@
 SETLOCAL
 
-set PATH=%CD%\visualc\Release;%PATH%
+set EXE_PATH=visualc\Release
+set PATH=%CD%\%EXE_PATH%;%PATH%
 
 set fail_count=0
 
-cd %CD%\test
-
 unit_tests.exe
-if %ERRORLEVEL%=="0" goto tester-testsuite.lua
+if %ERRORLEVEL%==0 goto tester-testsuite.lua
 @echo unit-tests failed.
 set fail_count=1
 
 :tester-testsuite.lua
-tester.exe tester-testsuite.lua
-if NOT %ERRORLEVEL%=="0" goto testsuite.lua
+%EXE_PATH%\tester.exe tester-testsuite.lua
+if %ERRORLEVEL%==0 goto testsuite.lua
 @echo tester-testsuite.lua failed.
-if %fail_count%=="1" set fail_count=2
-if %fail_count%=="0" set fail_count=1
+if %fail_count%==1 set fail_count=2
+if %fail_count%==0 set fail_count=1
 
 :testsuite.lua
-tester.exe testsuite.lua
-if NOT %ERRORLEVEL%=="0" goto nomore
+%EXE_PATH%\tester.exe testsuite.lua
+if %ERRORLEVEL%==0 goto nomore
 @echo testsuite.lua failed.
-if %fail_count%=="2" set fail_count=3
-if %fail_count%=="1" set fail_count=2
-if %fail_count%=="0" set fail_count=1
+if %fail_count%==2 set fail_count=3
+if %fail_count%==1 set fail_count=2
+if %fail_count%==0 set fail_count=1
 
 :nomore
-if %fail_count%=="0" goto end
+if %fail_count%==0 goto end
 
 @rem return an error code
 @echo ===============================
