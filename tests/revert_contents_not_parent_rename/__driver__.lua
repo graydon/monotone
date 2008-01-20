4ab225a6b06e552f1f926e50fe6da41e680759f3
me <me@thomaskeller.biz>
2008-01-20 23:04:41 +0000

    * tests/restricted_revert_bug renamed to tests/revert_contents_not_parent_rename: no longer a bug, remove the xfail * tests/revert_drop_not_rename: remove the xfail * cmd_ws_commit.cc: improve path handling of revert by using the new path of the parent directory of a rename or alternatively the remembered intermediate path of any previously reverted node, which become the new_path as soon as _MTN/revision is written out later on * NEWS: note the bug fixes

--- tests/revert_contents_not_parent_rename/__driver__.lua
+++ tests/revert_contents_not_parent_rename/__driver__.lua
@@ -1,6 +1,8 @@
 mtn_setup()
 
--- this bug appears to be related to tests/restricted_diff_bug
+-- this was a bug until 0.38 and appeared to be related to
+-- tests/restricted_diff_bug, however was resolved by some clever
+-- path handling tricks in revert
 
 mkdir("dir1")
 addfile("dir1/test.txt", "booya")
@@ -13,11 +15,10 @@ writefile("dir2/test.txt", "boohoo")
 
 -- presumably this should only revert the content of test.txt
 -- because it only includes that node
--- note that it is the parents name that has changed
 
 check(mtn("revert", "dir2/test.txt"), 0, false, false)
 
 reverted=sha1("dir2/test.txt")
-xfail(original == reverted)
+check(original == reverted)
 
 
--- tests/revert_drop_not_rename/__driver__.lua
+++ tests/revert_drop_not_rename/__driver__.lua
@@ -1,11 +1,8 @@
 
 --
--- this is a bug report for a weird behaviour with revert
--- when a dropped (previously renamed) node should be reverted
--- and the rename has happened on one of the parents of the
--- node
--- see http://colabti.de/irclogger/irclogger_log/monotone?date=2008-01-18,Fri&sel=92#l174
--- for more details
+-- Check if a dropped node is properly reverted to its new path
+-- which is determined by a not reverted rename of an ancestor
+-- (old_dir -> new_dir)
 --
 
 mtn_setup()
@@ -18,9 +15,10 @@ commit()
 check(mtn("mv", "old_dir", "new_dir"), 0, false, false)
 check(mtn("drop", "new_dir/a"), 0, false, false)
 
--- this is the actual command that misbehaves, in the way
--- that it does not re-create a under new_dir/a, but
--- old_dir/a, which fails, because old_dir is an old node
--- and renamed
+--
+-- this should re-create a under new_dir/, but not under the
+-- old path old_dir/a, i.e. mtn status should not return nonzero
+-- because it found missing files
+--
 check(mtn("revert", "old_dir/a"), 0, false, false)
-xfail(mtn("status"), 0, false, false)
+check(mtn("status"), 0, false, false)
