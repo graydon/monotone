528d629d835cfd5ed70a8aea5a6a4b3ac4015a8e
Richard Levitte <richard@levitte.org>
2008-05-22 14:59:50 +0000

    * examples/display_branches.lua: Use a more modern way to define 	netsync notifier functions.

--- examples/display_branches.lua
+++ examples/display_branches.lua
@@ -6,47 +6,53 @@
 --
 -- Released as public domain
 
-netsync_branches = {}
-function note_netsync_start(nonce)
-   netsync_branches[nonce] = {}
-end
-
-function _note_netsync_cert_received(rev_id,key,name,value,nonce)
-   if name == "branch" then
-      if netsync_branches[nonce][value] == nil then
-         netsync_branches[nonce][value] = 1
-      else
-         netsync_branches[nonce][value] = netsync_branches[nonce][value] + 1
+do
+   netsync_branches = {}
+   function _note_netsync_cert_received(rev_id,key,name,value,nonce)
+      if name == "branch" then
+	 if netsync_branches[nonce][value] == nil then
+	    netsync_branches[nonce][value] = 1
+	 else
+	    netsync_branches[nonce][value] = netsync_branches[nonce][value] + 1
+	 end
       end
    end
-end
 
-function note_netsync_revision_received(new_id,revision,certs,nonce)
-   for _, item in pairs(certs)
-   do
-      _note_netsync_cert_received(new_id,item.key,item.name,item.value,nonce)
-   end
-end
+   push_netsync_notifier(
+      {
+	 [start] =
+	    function (nonce)
+	       netsync_branches[nonce] = {}
+	    end,
+	 [revision_received] =
+	    function (new_id,revision,certs,nonce)
+	       for _, item in pairs(certs)
+	       do
+		  _note_netsync_cert_received(new_id,item.key,item.name,item.value,nonce)
+	       end
+	    end,
+	 [cert_received] =
+	    function (rev_id,key,name,value,nonce)
+	       _note_netsync_cert_received(rev_id,key,name,value,nonce)
+	    end,
+	 ["end"] =
+	    function (nonce, status)
+	       -- only try to display results if we got
+	       -- at least partial contents
+	       if status > 211 then
+		  return
+	       end
 
-function note_netsync_cert_received(rev_id,key,name,value,nonce)
-   _note_netsync_cert_received(rev_id,key,name,value,nonce)
-end
-
-function note_netsync_end(nonce, status)
-   -- only try to display results if we got
-   -- at least partial contents
-   if status > 211 then
-        return
-   end
-
-   local first = true
-   for item, amount in pairs(netsync_branches[nonce])
-   do
-      if first then
-	 io.stderr:write("Affected branches:\n")   
-	 first = false
-      end
-      io.stderr:write("  ",item,"  (",amount,")\n")   
-   end
-   netsync_branches[nonce] = nil
+	       local first = true
+	       for item, amount in pairs(netsync_branches[nonce])
+	       do
+		  if first then
+		     io.stderr:write("Affected branches:\n")   
+		     first = false
+		  end
+		  io.stderr:write("  ",item,"  (",amount,")\n")   
+	       end
+	       netsync_branches[nonce] = nil
+	    end
+      })
 end
