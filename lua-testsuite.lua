4416a15b396ad036fa7d4c10906daa4076a20231
mtn-dev <mtn-dev@zackw.users.panix.com>
2008-05-25 04:12:56 +0000

    * unix/tester-check-net.c, win32/tester-check-net.c: New files.  * Makefile.am: Build 'check_net' helper binary during 'make check'.  * lua-testsuite.lua: Use 'check-net' to decide if network tests are ok.  * tests/common/netsync.lua: skip_if() any test that includes this file    when network tests are not ok.  * debian/changelog: this fixes Debian bug #474280.

--- lua-testsuite.lua
+++ lua-testsuite.lua
@@ -1,6 +1,7 @@
 #!./tester
 
 monotone_path = nil
+no_network_tests = false
 
 function safe_mtn(...)
   if monotone_path == nil then
@@ -356,6 +357,36 @@ function prepare_to_run_tests (P)
       return 1
    end
    unlogged_remove(d)
+
+   -- Several tests require the ability to run a network server on
+   -- the loopback interface, and connect to it from another process
+   -- on this computer.  Unlike the above, we just skip those tests
+   -- (loudly) if we can't do that.  Verifying that this is possible
+   -- requires a helper program.
+
+   local checknet = getpathof("check_net")
+   if checknet ~= nil then
+      writefile_q("in", nil)
+      prepare_redirect("in", "out", "err")
+      local status = execute(checknet)
+      local out = readfile_q("out")
+      local err = readfile_q("err")
+
+      if status == 0 and err == "" and out == "" then
+	 logfile:write("check_net: Can use the loopback interface.\n")
+      else
+	 logfile:write(string.format("check_net: failed with status %d\n"..
+				     "stdout:\n%s\nstderr:\n%s\n",
+				     status, out, err))
+	 P("warning: network unavailable, skipping network server tests\n")
+	 no_network_tests = true
+      end
+   else
+      P("warning: check_net helper is missing, skipping network server tests\n")
+      no_network_tests = true
+   end
+
+   -- Record the full version of monotone under test in the logfile.
 
    monotone_path = getpathof("mtn")
    if monotone_path == nil then monotone_path = "mtn" end
---
+++ win32/tester-check-net.c
@@ -0,0 +1,6 @@
+/* Stub - needs implementing.  See lua-testsuite.lua (very end) for rationale,
+   and unix/tester-check-net.c for behavior to emulate.  */
+int main(void)
+{
+  return 0;
+}
